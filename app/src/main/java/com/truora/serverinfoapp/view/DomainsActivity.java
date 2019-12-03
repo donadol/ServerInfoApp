ad00d1626e5b31d3d4d6421510347c337ef3b6df
Laura Donado <ldonado11@gmail.com>
2019-12-02 22:35:16 -0500

    created activities

---
+++ app/src/main/java/com/truora/serverinfoapp/view/DomainsActivity.java
@@ -0,0 +1,16 @@
+package com.truora.serverinfoapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.truora.serverinfoapp.R;
+
+public class DomainsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domains);
+    }
+}
